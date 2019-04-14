d9479664ab1b939c201731a59ffedd907628f8f0
wehart <whart222@gmail.com>
2019-04-14 09:59:54 -0600

    Updating package documentation

--- pao/__init__.py
+++ pao/__init__.py
@@ -1,19 +1,24 @@
-#
-# Importing PAO initializes the Pyomo environment, if it
-# is necessary, and then registers the pao plugins.
-#
-# NOTE:  We assume that a user will never import symbols from
-# pao directly:
-#
-#   from pao import *
-#
-# Instead, users should import symbols directly from pao
-# sub-packages:
-#
-#   from pao.bilevel import *
-#
+"""
+pao package
+
+Defines modeling techniques and solvers for representing optimization
+problems with adversarial behavior.  PAO currently supports bilevel
+programming.
+
+Importing PAO initializes the Pyomo environment and then registers
+the pao plugins.  We assume that a user will never import symbols from
+pao directly:
+
+    from pao import *
+
+Instead, users should import symbols directly from pao sub-packages:
+
+    from pao.bilevel import *
+"""
+
+__all__ = ()
+
 import pyomo.environ
 import pao.duality.plugins
 import pao.bilevel.plugins
 pao.bilevel.plugins.load()
-
