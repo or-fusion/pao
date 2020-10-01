b5652930f35f731af57d04f19ff7236a91ab965b
whart222 <whart222@gmail.com>
2020-10-01 10:17:49 -0600

    Set integer vars with integer values

--- pao/lbp/soln_manager.py
+++ pao/lbp/soln_manager.py
@@ -19,16 +19,16 @@ class LBP_SolutionManager(object):
         for j in lbp.U.xR:
             lbp.U.xR.values[j] = sum(pe.value(UxR[v]) * c for v,c in self.multipliers_UxR[j])
         for j in lbp.U.xZ:
-            lbp.U.xZ.values[j] = sum(pe.value(UxZ[v]) * c for v,c in self.multipliers_UxZ[j])
+            lbp.U.xZ.values[j] = round(sum(pe.value(UxZ[v]) * c for v,c in self.multipliers_UxZ[j]))
         if UxB is not None:
             if len(UxB) == 0:
                 # Binaries are at the end of the integers
                 nxZ = len(lbp.U.xZ)
                 for j in lbp.U.xB:
-                    lbp.U.xB.values[j] = pe.value(UxZ[nxZ+j])
+                    lbp.U.xB.values[j] = round(pe.value(UxZ[nxZ+j]))
             else:
                 for j in lbp.U.xB:
-                    lbp.U.xB.values[j] = pe.value(UxB[j])
+                    lbp.U.xB.values[j] = round(pe.value(UxB[j]))
         #
         # TODO: Handle multiple subproblems within Pyomo models
         #
@@ -37,16 +37,16 @@ class LBP_SolutionManager(object):
             for j in lbp.L[i].xR:
                 lbp.L[i].xR.values[j] = sum(pe.value(LxR[v]) * c for v,c in self.multipliers_LxR[i][j])
             for j in lbp.L[i].xZ:
-                lbp.L[i].xZ.values[j] = sum(pe.value(LxZ[v]) * c for v,c in self.multipliers_LxZ[i][j])
+                lbp.L[i].xZ.values[j] = round(sum(pe.value(LxZ[v]) * c for v,c in self.multipliers_LxZ[i][j]))
             if LxB is not None:
                 if len(LxB) == 0:
                     # Binaries are at the end of the integers
                     nxZ = len(lbp.L[i].xZ)
                     for j in lbp.L[i].xB:
-                        lbp.L[i].xB.values[j] = pe.value(LxZ[nxZ+j])
+                        lbp.L[i].xB.values[j] = round(pe.value(LxZ[nxZ+j]))
                 else:
                     for j in lbp.L[i].xB:
-                        lbp.L[i].xB.values[j] = pe.value(LxB[j])
+                        lbp.L[i].xB.values[j] = round(pe.value(LxB[j]))
 
     def load_from(self, data):
         # TODO - should we copy the data from a Pyomo model?  or a Pyomo results object?
