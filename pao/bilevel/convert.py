6ab5703407aa96b267c6f53c00115257553eaf53
wehart <whart222@gmail.com>
2020-11-19 12:10:16 -0700

    Updating conversion logic

--- pao/bilevel/convert.py
+++ pao/bilevel/convert.py
@@ -76,44 +76,35 @@ class Node(object):
         """
         Initialize the level object...
         """
+        level.x._resize(nxR=len(self.xR), nxZ=len(self.xZ), nxB=len(self.xB))
         #
         # xR
         #
-        if len(self.xR) > 0:
-            level.xR.resize(len(self.xR))
-            lb = [np.NINF]*len(self.xR)
-            ub = [np.PINF]*len(self.xR)
-            for i in self.xR:
-                vid = self.xR[i]
-                val = var[vid].lb
-                if val is not None:
-                    lb[i] = val
-                val = var[vid].ub
-                if val is not None:
-                    ub[i] = val
-            level.xR.lower_bounds = lb
-            level.xR.upper_bounds = ub
+        for i in self.xR:
+            vid = self.xR[i]
+            val = var[vid].lb
+            if val is not None:
+                level.x.lower_bounds[i] = val
+            val = var[vid].ub
+            if val is not None:
+                level.x.upper_bounds[i] = val
         #
         # xZ
         #
-        if len(self.xZ) > 0:
-            level.xZ.resize(len(self.xZ))
-            lb = [np.NINF]*len(self.xZ)
-            ub = [np.PINF]*len(self.xZ)
-            for i in self.xZ:
-                vid = self.xZ[i]
-                val = var[vid].lb
-                if val is not None:
-                    lb[i] = val
-                val = var[vid].ub
-                if val is not None:
-                    ub[i] = val
-            level.xZ.lower_bounds = lb
-            level.xZ.upper_bounds = ub
+        for i in self.xZ:
+            vid = self.xZ[i]
+            val = var[vid].lb
+            if val is not None:
+                level.x.lower_bounds[i+level.x.nxR] = val
+            val = var[vid].ub
+            if val is not None:
+                level.x.upper_bounds[i+level.x.nxR] = val
         #
         # xB
         #
-        level.xB.resize(len(self.xB))
+        for i in self.xB:
+            level.x.lower_bounds[i+level.x.nxR+level.x.nxZ] = 0
+            level.x.upper_bounds[i+level.x.nxR+level.x.nxZ] = 1
 
     def initialize_level(self, level, inequalities, var, vidmap, levelmap):
         #
@@ -126,62 +117,33 @@ class Node(object):
                 level.minimize = False
             level.d = pe.value(repn.constant)
 
-            c_xR = {}
-            c_xZ = {}
-            c_xB = {}
-            for i in levelmap:
-                c_xR[i] = {}
-                c_xZ[i] = {}
-                c_xB[i] = {}
+            c = {}
+            for j in levelmap:
+                node = levelmap[j]
+                c[j] = [0]*node.x.num
 
-            for i,c  in enumerate(repn.linear_coefs):
+            for i,val  in enumerate(repn.linear_coefs):
                 vid = id(repn.linear_vars[i])
                 t, nid, j = vidmap[vid]
+                node = levelmap[j]
                 #print(t,nid,j)
                 if t == 0:
-                    c_xR[nid][j] = pe.value(c)
+                    c[nid][j] = pe.value(val)
                 elif t == 1:
-                    c_xZ[nid][j] = pe.value(c)
+                    c[nid][j+node.nxR] = pe.value(val)
                 elif t == 2:
-                    c_xB[nid][j] = pe.value(c)
+                    c[nid][j+node.nxR+node.nxZ] = pe.value(val)
 
             for j in levelmap:
                 node = levelmap[j]
-                if len(c_xR[j]) > 0:
-                    tmp = [0]*len(node.xR)
-                    for i,v in c_xR[j].items():
-                        tmp[i] = v
-                    if j == 0:
-                        level.c.U.xR = tmp
-                    else:
-                        level.c.L[j-1].xR = tmp
-                if len(c_xZ[j]) > 0:
-                    tmp = [0]*len(node.xZ)
-                    for i,v in c_xZ[j].items():
-                        tmp[i] = v
-                    if j == 0:
-                        level.c.U.xZ = tmp
-                    else:
-                        level.c.L[j-1].xZ = tmp
-                if len(c_xB[j]) > 0:
-                    tmp = [0]*len(node.xB)
-                    for i,v in c_xB[j].items():
-                        tmp[i] = v
-                    if j == 0:
-                        level.c.U.xB = tmp
-                    else:
-                        level.c.L[j-1].xB = tmp
+                level.c[node.id] = c[node.id]
         #
         # A
         #
         if len(self.crepn) > 0:
-            A_xR = {}
-            A_xZ = {}
-            A_xB = {}
+            A = {}
             for i in levelmap:
-                A_xR[i] = {}
-                A_xZ[i] = {}
-                A_xB[i] = {}
+                A[i] = {}
             b = []
             nrows = len(self.crepn)
 
@@ -190,43 +152,24 @@ class Node(object):
                 for i,c  in enumerate(repn.linear_coefs):
                     vid = id(repn.linear_vars[i])
                     t, nid, j = vidmap[vid]
+                    node = levelmap[j]
                     if t == 0:
-                        A_xR[nid][k,j] = pe.value(c)
+                        A[nid][k,j] = pe.value(c)
                     elif t == 1:
-                        A_xZ[nid][k,j] = pe.value(c)
+                        A[nid][k,j+node.x.nxR] = pe.value(c)
                     elif t == 2:
-                        A_xB[nid][k,j] = pe.value(c)
+                        A[nid][k,j+node.x.nxR+node.x.nxZ] = pe.value(c)
                 b.append(self.crepn[k][1] - repn.constant)
 
             level.b = b
 
             for j in levelmap:
                 node = levelmap[j]
-                if len(A_xR[j]) > 0:
-                    mat = dok_matrix((nrows, len(node.xR)))
-                    for key, val in A_xR[j].items():
+                if len(A[j]) > 0:
+                    mat = dok_matrix((nrows, node.x.num))
+                    for key, val in A[j].items():
                         mat[key] = val
-                    if j == 0:
-                        level.A.U.xR = mat
-                    else:
-                        level.A.L[j-1].xR = mat
-                if len(A_xZ[j]) > 0:
-                    mat = dok_matrix((nrows, len(node.xZ)))
-                    for key, val in A_xZ[j].items():
-                        mat[key] = val
-                    if j == 0:
-                        level.A.U.xZ = mat
-                    else:
-                        level.A.L[j-1].xZ = mat
-                if len(A_xB[j]) > 0:
-                    mat = dok_matrix((nrows, len(node.xB)))
-                    for key, val in A_xB[j].items():
-                        mat[key] = val
-                    if j == 0:
-                        level.A.U.xB = mat
-                    else:
-                        level.A.L[j-1].xB = mat
-                
+                    level.A[node.id] = mat
             
 
 def negate_repn(repn):
@@ -383,19 +326,21 @@ class PyomoSubmodel_SolutionManager_LBP(object):
             v = self.var[vid]
             t, nid, j = self.vidmap[vid]
             if nid == 0:
+                U = lbp.U
                 if t == 0:
-                    v.value = lbp.U.xR.values[j]
+                    v.value = lbp.U.x.values[j]
                 elif t == 1:
-                    v.value = lbp.U.xZ.values[j]
+                    v.value = lbp.U.x.values[j+U.x.nxR]
                 elif t == 2:
-                    v.value = lbp.U.xB.values[j]
+                    v.value = lbp.U.x.values[j+U.x.nxR+U.x.nxZ]
             else:
+                L = lbp.U.LL[nid-1]
                 if t == 0:
-                    v.value = lbp.L[nid-1].xR.values[j]
+                    v.value = lbp.U.LL[nid-1].x.values[j]
                 elif t == 1:
-                    v.value = lbp.L[nid-1].xZ.values[j]
+                    v.value = lbp.U.LL[nid-1].x.values[j+L.x.nxR]
                 elif t == 2:
-                    v.value = lbp.L[nid-1].xB.values[j]
+                    v.value = lbp.U.LL[nid-1].x.values[j+L.x.nxR+L.x.nxZ]
 
 
 def convert_pyomo2LinearBilevelProblem1(model, *, determinism=1, inequalities=True):
@@ -438,11 +383,12 @@ def convert_pyomo2LinearBilevelProblem1(model, *, determinism=1, inequalities=Tr
     #
     levelmap = {}
     M = LinearBilevelProblem()
-    U = M.add_upper()
+    U = M.add_upper(id=0)
+    L = {}
     levelmap[0] = U 
     tree.initialize_level_vars(U, inequalities, var)
     for i,c in enumerate(tree.children):
-        L = M.add_lower()
+        L[i] = U.add_lower(id=i+1)
         levelmap[i+1] = L[i]
         c.initialize_level_vars(L[i], inequalities, var)
     
