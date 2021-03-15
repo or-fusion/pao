f8d595dd13299d742c605aea9931321e9f754d65
whart222 <whart222@gmail.com>
2021-03-14 20:12:12 -0600

    Adding simple quadratic bilevel problem

---
+++ pao/lbp/examples/barguel.py
@@ -0,0 +1,44 @@
+#
+# Simple interdiction problem solvable through
+# lower level dualization and MPEC transformation
+#
+# Note that this is a quadratic bilevel problem
+#
+# This model has an optimal objective value of 0
+# To see this, fix u and optimize the lower level
+#
+# DUALIZED:
+# min   v1 + v2 + u
+# s.t.  u1 + v1 >= 1    : x
+#       -u*u1 + v2 >= 0  : y
+#       v1, v2, >= 0
+#       u1 unconstrained
+#
+
+from pao.lbp import *
+
+def create():
+    M = QuadraticMultilevelProblem()
+
+    U = M.add_upper(nxB=1)
+    L = U.add_lower(nxR=2)
+    L.equalities = True
+
+    U.c[U] = [1]
+    U.c[L] = [1, 0]
+
+    L.c[U] = [1]
+    L.c[L] = [1, 0]
+
+    L.A[L] = [[1, 0]]
+    L.Q[U,L] = (1,1,2), {(0,0,1):1}
+    L.b    = [0]
+
+    return M
+
+
+if __name__ == "__main__":          #pragma: no cover
+    M = create()
+    opt = SolverFactory('pao.lbp.FA')
+    opt.solve(M)
+    M.print()
